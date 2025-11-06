b5c7427970ddb1e3b8e6df18ae59128362c0e64b
anandkprajapati123 <anandkprajapati123@gmail.com>
2025-11-06 15:39:04 +0530

    added

---
+++ Front-end/first-react-app/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>Address: 123 Foodie Lane, Flavor Town, USA</p>
+    </footer>
+  );
+}
+
+export default Footer;

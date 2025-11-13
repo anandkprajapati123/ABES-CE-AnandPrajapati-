4cb7a350b38a8dc6fa0c7b2337eca68d1ba73e5d
anandkprajapati123 <anandkprajapati123@gmail.com>
2025-11-13 15:29:38 +0530

    Weather app

--- 05 Front-end/first-react-app/src/App.jsx
+++ 05 Front-end/first-react-app/src/App.jsx
@@ -1,27 +1,24 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-// import Navbar from './components/Navbar';
-// import Home from './components/Home';
-// import Card from './components/Card';
-// import Footer from './components/Footer';
-// import Count from './components/Count';
-import Link1 from './components/Link1';
+import React, { useEffect, useState } from 'react'
+import Fashion from './components/Fashion'
 
 function App() {
+  const[books,setBooks]=useState([])
+  useEffect(()=>{
+    fetch('https://fakestoreapi.com/products')
+    .then(res=>res.json())
+    .then(data=>{
+      setBooks(data)
+    })
+  },[])
   return (
-  <div>
-   {/* <Navbar/>
-    <br />
-    <Home/>
-     <br />
-    <Card/>
-    <br />
-    <Footer/>
-    <br /> */}
-    {/* <Count /> */}
-
-    <Link1 />
-  </div>
-  );
+    <div>
+      {
+        books.map((b,i)=>(
+          <Fashion key={i} props={b}/>
+        ))
+      }
+    </div>
+  )
 }
 
-export default App;
+export default App
